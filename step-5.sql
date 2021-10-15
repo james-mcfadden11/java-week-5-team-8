44d319108f55c312390c1ec065c1492bfa21f6ac
James McFadden <jamesmcfadden111@gmail.com>
2021-10-15 16:39:29 -0400

    Step 5 complete

--- step-5.sql
+++ step-5.sql
@@ -9,13 +9,15 @@
   description: Ohiopyle State Park is a Pennsylvania state park on 19,052 acres in Dunbar, Henry Clay and Stewart Townships, Fayette County, Pennsylvania in the United States. The focal point of the park is the more than 14 miles of the Youghiogheny River Gorge that passes through the park.
   ------------------------------
 */
-
+insert into park (name, location, establish_date, area, visitors, description)
+values ('Ohiopyle State Park', 'Pennsylvania', '1965-01-01', 19052, 1000000, 'Ohiopyle State Park is a Pennsylvania state park on 19,052 acres in Dunbar, Henry Clay and Stewart Townships, Fayette County, Pennsylvania in the United States. The focal point of the park is the more than 14 miles of the Youghiogheny River Gorge that passes through the park.');
 
 /*
   STEP TWO: You just found out that there was an error with the park data. Please update the park visitors to 1.5 million anually.
-
 */
-
+update park
+set visitors = 1500000
+where name = 'Ohiopyle State Park';
 
 /*
  STEP THREE: Insert new campground with the following data:
@@ -27,7 +29,8 @@
   daily_fee: 95.00
   ------------------------------------------------------------
 */
-
+insert into campground (park_id, name, open_from_mm, open_to_mm, daily_fee)
+values (4, 'Youghiogheny', '01', '12', 95.00);
 
 /*
  STEP FOUR: Insert 3 new sites with the following data:
@@ -36,11 +39,10 @@
   site_number: 624, campground_id: 8
   site_number: 625, campground_id: 8
  ------------------------------------------------------------
-
  > campground_id 8 should be the id of the campground you just added 'Youghiogheny'
-
 */
-
+insert into site (site_number, campground_id)
+values (623, 8), (624, 8), (625, 8);
 
 /*
  STEP FIVE: Insert 3 reservations, 1 for each site with the following data:
@@ -49,18 +51,23 @@
   site_id: 624, name: Parker Family, from_date: today + 11 days, to_date: today + 20 days
   site_id: 625, name: Kent Family, from_date: today + 12 days, to_date: today + 20 days
 ------------------------------------------------------------------------------------
-
 */
-
+insert into reservation (site_id, name, from_date, to_date)
+values (623, 'Wayne Family', NOW() + INTERVAL '10 DAY', NOW() + INTERVAL '20 DAY'), 
+(624, 'Parker Family', NOW() + INTERVAL '11 DAY', NOW() + INTERVAL '20 DAY'),
+(625, 'Kent Family', NOW() + INTERVAL '12 DAY', NOW() + INTERVAL '20 DAY');
 
 /*
- STEP SIX: The Wayne Family called and asked if they could change their reservation to today. Update the from_date to today and the to_date to a week from today.
-
+ STEP SIX: The Wayne Family called and asked if they could change their reservation to today. 
+ Update the from_date to today and the to_date to a week from today.
  */
-
+update reservation
+set from_date = NOW(), to_date = NOW() + INTERVAL '7 DAY'
+where site_id = (select site_id from reservation where name = 'Wayne Family' and from_date = '10/25/2021');
 
 /*
  STEP SEVEN: The Kent family called and they would like to cancel their reservation. Delete the reservation for Kent Family.
-
 */
+delete from reservation
+where site_id = (select site_id from reservation where name = 'Kent Family' and from_date = '10/27/2021');
 
