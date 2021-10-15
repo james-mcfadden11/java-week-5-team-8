57bcc96c78ef512fa8f04417746c84a4ca2edd08
James McFadden <jamesmcfadden111@gmail.com>
2021-10-15 15:17:10 -0400

    Step 4 complete

--- step-4.sql
+++ step-4.sql
@@ -1,11 +1,23 @@
 -- select the park name, campground name, open_from_mm, open_to_mm & daily_fee ordered by park name and then campground name
-
+select park.name as park_name, campground.name as campground_name, open_from_mm, open_to_mm, daily_fee
+from park
+join campground using(park_id)
+order by park.name, campground.name;
 
 -- select the park name and the total number of campgrounds for each park ordered by park name
+select park.name, count(campground_id) as number_of_campgrounds
+from park
+join campground using(park_id)
+group by park.name
+order by park.name;
 
-
-
--- select the park name, campground name, site number, max occupancy, accessible, max rv length, utilities where the campground name is 'Blackwoods'
+-- select the park name, campground name, site number, max occupancy, accessible, max rv length, utilities 
+--where the campground name is 'Blackwoods'
+select park.name as park_name, campground.name as campground_name, site_number, max_occupancy, accessible, max_rv_length, utilities
+from park
+join campground using(park_id)
+join site using(campground_id)
+where campground.name = 'Blackwoods';
 
 /*
   select park name, campground, total number of sites (column alias 'number_of_sites') ordered by park
@@ -19,11 +31,18 @@
     Cuyahoga Valley	The Unnamed Primitive Campsites	5
   -------------------------------------------------
 */
-
+select park.name as park_name, campground.name as campground_name, count(site_id) as number_of_sites
+from campground
+join park using(park_id)
+join site using (campground_id)
+group by park.name, campground.name
+order by park.name;
 
 -- select site number, reservation name, reservation from and to date ordered by reservation from date
-
-
+select site_number, reservation.name, from_date, to_date
+from site
+join reservation using(site_id)
+order by from_date;
 
 /*
   select campground name, total number of reservations for each campground ordered by total reservations desc
@@ -37,6 +56,12 @@
     Juniper Group Site	4
   -------------------------------------------------
 */
+select campground.name as campground_name, count(reservation_id)
+from campground
+join site using(campground_id)
+join reservation using(site_id)
+group by campground.name
+order by count(reservation_id) desc;
 
 
 
